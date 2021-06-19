05c38e48de793052e7fbd11b04e07b666936558a
Bijay Budhathoki <bijayabudhathoki98@gmail.com>
2021-06-19 10:15:44 +0545

    banner fixing

--- src/containers/Banner/Banner.tsx
+++ src/containers/Banner/Banner.tsx
@@ -2,10 +2,12 @@ import { Button } from '@/components';
 import React from 'react';
 export const Banner = (): JSX.Element => {
     return (
-        <div className="flex flex-col items-center justify-center py-12 ">
+        <div className="flex flex-col items-center justify-center py-10 ">
             <div className="flex flex-col items-center justify-center w-2/4 gap-8 text-center">
-                <h3 className="text-6xl font-medium leading-snug">Top Quality Digital Products To Explore</h3>
-                <p className="text-2xl leading-relaxed">
+                <h3 className="text-5xl font-bold leading-tight tracking-wide">
+                    Top Quality Digital Products To Explore
+                </h3>
+                <p className="text-2xl leading-relaxed text-gray-600">
                     Get your blood tests delivered at let home collect sample from the victory of the managements that
                     supplies best design system guidelines ever.
                 </p>
